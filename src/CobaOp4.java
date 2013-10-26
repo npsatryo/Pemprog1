2fc77b591bb87e20f1a82c26425c1a414ecefcdb
npsatryo <np.satryo@gmail.com>
2013-10-26 20:59:58 +0700

    comm04

---
+++ src/CobaOp4.java
@@ -0,0 +1,12 @@
+package Latihan.sesi2.bin;
+public class CobaOp4
+{
+	public static void main(String[] Xx)
+	{
+		int x=100;
+		System.out.println("Nilai Binary  [bin] 100: "+Integer.parseInt("100",2));
+		System.out.println("Nilai Octal   [Oct] 100: "+Integer.parseInt("100",8));
+		System.out.println("Nilai Decimal [Dec] 100: "+Integer.parseInt("100",10));
+		System.out.println("Nilai Hexa    [Hex] 100: "+Integer.parseInt("100",16));
+	}
+}
